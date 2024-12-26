5278431506d34cfff63cb07709a31ddf6039b952
Unay Santisteban <usantisteban@othercode.io>
2024-12-26 18:30:59 +0100

    Feature/Frontend template directories.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
+import {resolve} from 'path';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+const frontAppPath = 'resources/templates/tailwindcss/js';
+
 export default defineConfig({
+    resolve: {
+        alias: {
+            // override the default alias of '@' to point to the frontAppPath
+            '@': resolve(__dirname, frontAppPath),
+        }
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: frontAppPath.concat('/app.js'),
             refresh: true,
         }),
         vue({

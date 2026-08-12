37f95262bf3e066f3b85ff4fb476cc2d79154b6a
Unay Santisteban <usantisteban@othercode.io>
2026-08-12 11:18:28 +1000

    chore: #54 add an APITokens aggregate for Sanctum

---
+++ app/IdentityAndAccess/APITokens/Domain/APIToken.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\IdentityAndAccess\APITokens\Domain;
+
+use Laravel\Sanctum\PersonalAccessToken;
+
+/**
+ * Class APIToken
+ *
+ * Owns the token table instead of publishing Sanctum's migration, which
+ * declares a `morphs('tokenable')` and would therefore store a bigint that
+ * cannot hold the uuid identifiers this application gives its users.
+ *
+ * Registered via Sanctum::usePersonalAccessTokenModel() in the bounded
+ * context service provider.
+ *
+ * @property string $tokenable_type
+ * @property string $tokenable_id
+ * @property string $name
+ * @property string $token
+ * @property string|null $abilities
+ *
+ * @author Unay Santisteban <usantisteban@othercode.io>
+ */
+class APIToken extends PersonalAccessToken
+{
+    protected $table = 'api_tokens';
+}
